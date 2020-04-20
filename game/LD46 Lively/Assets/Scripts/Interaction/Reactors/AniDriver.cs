553cfda2e2be12bee66d44677ed895b119cb483b
CJ Gladback <33671915+cjgladback@users.noreply.github.com>
2020-04-20 00:48:12 -0400

    last night augh
    yeetus deleetus but not really

---
+++ game/LD46 Lively/Assets/Scripts/Interaction/Reactors/AniDriver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AniDriver : StateReactor
+{
+    Animator anim;
+    protected override void Awake()
+    {
+        base.Awake();
+        anim = GetComponent<Animator>();
+    }
+
+    public override void React()
+    {
+        if (anim.GetBool("Moving") == false)
+        {
+            anim.SetBool("Moving", true);
+        } else
+        {
+            anim.SetBool("Moving", false);
+        }
+    }
+}
---
+++ game/LD46 Lively/Assets/Scripts/WalnutPlacer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using DG.Tweening;
+
+public class WalnutPlacer : MonoBehaviour
+{
+    public float walnutFall;
+    void Update()
+    {
+
+        NavMeshAgent agent = GetComponent("NavMeshAgent") as NavMeshAgent;
+        NavMeshHit closestHit;
+        if (NavMesh.SamplePosition(transform.position, out closestHit, 100f, NavMesh.AllAreas))
+        {
+            Sequence seq = DOTween.Sequence();
+            seq.Append(transform.DOMove(closestHit.position, walnutFall));
+
+            //transform.position = closestHit.position;
+            //agent.enabled = true;
+        }
+    }
+
+}
